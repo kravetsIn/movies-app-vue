da4661d4329898cb79b2efad802d908baa119b38
Vladyslav Kravets <hello.kravets@gmail.com>
2020-10-26 23:53:47 +0200

    Init Commit

---
+++ src/components/Loader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="isShowLoader" class="loader-wrap">
+    <BSpinner type="grow" variant="light" label="Loading..."></BSpinner>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "Loader",
+  computed: {
+    ...mapGetters(["isShowLoader"])
+  }
+};
+</script>
+
+<style scoped>
+.loader-wrap {
+  z-index: 999999;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
