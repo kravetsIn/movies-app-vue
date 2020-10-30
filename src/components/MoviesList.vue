de52c9946f00d429fefb65333e2b10db1b24db1e
Vladyslav Kravets <hello.kravets@gmail.com>
2020-10-30 21:17:31 +0200

    Added remove movie. Search muvies

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,12 +1,13 @@
 <template>
   <BContainer>
-    <h3 class="list-title">IMDB Top 250</h3>
+    <h3 class="list-title">{{ listTitle }}</h3>
     <BRow>
       <template v-if="isExist">
         <BCol cols="3" v-for="(movie, key) in list" :key="key">
           <MovieItem
             :movie="movie"
             @mouseover.native="onMouseOver(movie.Poster)"
+            @removeItem="onRemoveItem"
           />
         </BCol>
       </template>
@@ -19,6 +20,7 @@
 
 <script>
 import MovieItem from "./MovieItem";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "MoviesList",
@@ -32,13 +34,27 @@ export default {
     MovieItem
   },
   computed: {
+    ...mapGetters("movies", ["isSearch"]),
     isExist() {
       return Boolean(Object.keys(this.list).length);
+    },
+    listTitle() {
+      return this.isSearch ? "Search result" : "IMDB Top 250";
     }
   },
   methods: {
+    ...mapActions("movies", ["removeMovie"]),
     onMouseOver(poster) {
       this.$emit("changePoster", poster);
+    },
+    async onRemoveItem({ id, title }) {
+      const isConfirmed = await this.$bvModal.msgBoxConfirm(
+        `Are you sure delete ${title}?`
+      );
+
+      if (isConfirmed) {
+        this.removeMovie(id);
+      }
     }
   }
 };
